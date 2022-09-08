a3c44c34061625d6550d1b7aca516ed3e1b37980
Beza29 <bezaasrat29@gmail.com>
2022-09-08 06:12:55 -0700

    welo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-* main  - print the string in the put function
+*main - print the string in the put function
 *
-* description: using the main function
-* this program prints  "programming is like building  a multilingual puzzle "
-* Return: 0
+* Description: using the main function
+* this program prints  "Programming is like building a multilingual puzzle"
+*Return: 0
 */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
-
