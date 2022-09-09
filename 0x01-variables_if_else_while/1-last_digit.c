74716cb5f3f9c40bbc55e6ae4d3b7e2f4b32e740
Beza29 <bezaasrat29@gmail.com>
2022-09-09 12:47:00 -0700

    logiii

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
 * main - print if the number is postive, zero, or negative
@@ -8,11 +10,24 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+int n;
+int l;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+l = n % 10;
+
+if (l > 5)
 {
-	putchar(ch);
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
-putchar('\n');
+else if (l == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, l);
+}
+else
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
 return (0);
 }
