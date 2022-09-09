8dacba9bbcdf3a05b8ccdd9de0ab94dc1ec15e0b
Beza29 <bezaasrat29@gmail.com>
2022-09-09 12:23:22 -0700

    lohi

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 /**
- * main - printing numbers from 0-9 with commas and space between them
+ * main - print if the number is postive, zero, or negative
+ *
  * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
 int main(void)
 {
-	int c;
-
-	for (c = 48; c <= 57; c++)
-	{
-		putchar(c);
-		if (c != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+return (0);
 }
