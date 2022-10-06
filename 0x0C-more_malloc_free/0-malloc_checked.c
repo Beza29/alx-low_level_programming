8a19019c8a62498facc3ccc5600cf747f52e2f7e
Beza29 <bezaasrat29@gmail.com>
2022-10-06 10:46:38 -0700

    cvhhd

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,22 +1,11 @@
-/**
- * @file 0-malloc_checked.c
- * @author muhabeid
- * @brief - function that allocates memory using malloc.
- * @version 0.1
- * @date 2022-04-07
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: a pointer to the allocated memory
+ * malloc_checked - allocates memory using malloc
+ * @b: bytes allocate
+ * Return: pointer
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -8,6 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
+int _strlen(char *s);
 int _strlen(char *s);
 int is_digit(char *s);
 
