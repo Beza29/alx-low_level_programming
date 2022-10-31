aaf2e37c3ff1be0640d8a4296469dd70eb459d2a
Beza29 <bezaasrat29@gmail.com>
2022-10-31 03:26:59 -0700

    baro

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,4 @@
-/*
- * File: 0-read_textfile.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -15,16 +10,17 @@
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	 ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (buffer ==  NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
@@ -41,4 +37,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(o);
 
 	return (w);
+
 }
